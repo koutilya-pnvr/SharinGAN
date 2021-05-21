4dcc816a0184da5555e10da626580569a62f13f7
Koutilya <koutilya40192@gmail.com>
2021-05-21 03:56:34 -0400

    Updated SSIM to preserve input shape

--- Monocular_Depth_Estimation/Geometric_consistency_loss.py
+++ Monocular_Depth_Estimation/Geometric_consistency_loss.py
@@ -9,12 +9,12 @@ def ssim(x, y):
         C1 = 0.01 ** 2
         C2 = 0.03 ** 2
         
-        mu_x = F.avg_pool2d(x, 3, 1)
-        mu_y = F.avg_pool2d(y, 3, 1)
+        mu_x = F.avg_pool2d(x, 3, 1, padding=1)
+        mu_y = F.avg_pool2d(y, 3, 1, padding=1)
 
-        sigma_x = F.avg_pool2d(x**2, 3, 1) - mu_x**2
-        sigma_y = F.avg_pool2d(y**2, 3, 1) - mu_y**2
-        sigma_xy = F.avg_pool2d(x*y, 3, 1) - mu_x*mu_y
+        sigma_x = F.avg_pool2d(x**2, 3, 1, padding=1) - mu_x**2
+        sigma_y = F.avg_pool2d(y**2, 3, 1, padding=1) - mu_y**2
+        sigma_xy = F.avg_pool2d(x*y, 3, 1, padding=1) - mu_x*mu_y
 
         SSIM_n = (2 * mu_x * mu_y + C1) * (2 * sigma_xy + C2)
         SSIM_d = (mu_x ** 2 + mu_y ** 2 + C1) * (sigma_x + sigma_y + C2)
